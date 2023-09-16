d608a6066dd264aec9cff98f77a0f76ebada630a
Ahmad Jafarov <ahmetleo56@gmail.com>
2023-09-16 14:29:08 +0400

    initial commit

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,28 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  NestInterceptor,
+  UseInterceptors,
+} from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { Observable, map } from 'rxjs';
+import { ClassConstructor } from 'class-transformer';
+
+export function Serialize<T>(dto: ClassConstructor<T>) {
+  return UseInterceptors(new ClassSerializerInterceptor(dto));
+}
+
+export class ClassSerializerInterceptor<T> implements NestInterceptor {
+  constructor(private readonly dto: ClassConstructor<T>) {}
+
+  intercept(context: ExecutionContext, handler: CallHandler): Observable<T> {
+    return handler.handle().pipe(
+      map((data: T) => {
+    
+        return plainToInstance(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
---
+++ src/users/users.controller.ts
@@ -0,0 +1,45 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UseInterceptors} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
+
+
+@Controller('auth')
+@Serialize(UserDto)
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    const { email, password } = body;
+    this.usersService.create(email, password);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    console.log("HANDLING THE DATA")
+    return this.usersService.findOne(parseInt(id));
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Patch('/:id')
+  updateUser(
+    @Param('id') id: string,
+    @Body() body: UpdateUserDto
+  ) {
+    return this.usersService.update(parseInt(id),body);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id))
+  }
+
+}
+
